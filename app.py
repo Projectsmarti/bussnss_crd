7772a821e9f0ab1ccef3957b7c9a72ed9ad1a529
Projectsmarti <151812630+Projectsmarti@users.noreply.github.com>
2024-10-16 14:40:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os, pandas as pd, google.generativeai as gem, csv, ast, streamlit as st
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-os.environ["GOOGLE_API_KEY"] = 'AIzaSyC4g4c-Dri_4CiRkSk0vN8w81r7OUj0iQs'
+os.environ["GOOGLE_API_KEY"] = 'AIzaSyDRqYSCNJzJG-OvtUGk4zKR8D-cmt9if9I'
 gem.configure(api_key=os.environ["GOOGLE_API_KEY"])
 # Configuration
 IMAGE_FOLDER = "uploaded_images"
